edae0cfe74ff77ff0bbdece96384716b2d8dbe77
mahiranand <mahiranand08@gmail.com>
2025-06-24 17:35:21 +0530

    minor bug fix.

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ async function startServer() {
     console.log("✅ Database connected.");
 
     await db.sequelize.sync();
-    console.log("✅ Database synced.");
+    console.log("Database synced.");
 
     app.listen(Number(PORT), "0.0.0.0", () => {
       console.log(`🚀 Server is running on http://0.0.0.0:${PORT}`);
